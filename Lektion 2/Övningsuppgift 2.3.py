6b64983bb27d197835c46c7cec13b45ba802fd2d
Navid Khosravi <nav.khosravi@gmail.com>
2023-10-06 22:06:25 +0200

    no real changes

--- Lektion 2/Övningsuppgift 2.3.py
+++ Lektion 2/Övningsuppgift 2.3.py
@@ -2,4 +2,4 @@ print("Hallå!")
 FirstName = input("Vad är ditt förnamn: ")
 LastName = input("Vad är ditt efternamn: ")
 
-print("Trevligt att träffas", FirstName, LastName)
+print("Trevlig att träffas", FirstName, LastName)
